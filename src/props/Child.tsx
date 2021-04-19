f61de5ca9be938f92f53eafff248c76f0e0743b6
IainLR <iainrobertzon@gmail.com>
2021-04-19 17:06:10 -0500

    function component definition and functions in interfaces

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,11 +1,35 @@
 interface ChildProps {
   color: string
+  onClick: () => void
 }
-
+// simplest implementation of interface
 // export const Child = (props: ChildProps) => {
 //     return <div>Hello</div>
 //   }
 
-export const Child = ({ color }: ChildProps) => {
-  return <div>{color}</div>
+// implementation with destructuring
+// however TS doesn't understand this is REACT component
+export const Child = ({ color, onClick }: ChildProps) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}>Click</button>
+    </div>
+  )
+}
+
+// defines function component with TS
+// TS is now aware this is React function component
+// aware of properties such as 'propTypes', 'contextTypes',
+// 'displayName', etc.
+// <ChildProps> tells TS the Child will recieve a type of
+// 'ChildProps'
+// .FC is short for .FunctionComponent. Interchangable
+export const ChildAsFC: React.FC<ChildProps> = ({ color, onClick }) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}>Click</button>
+    </div>
+  )
 }
