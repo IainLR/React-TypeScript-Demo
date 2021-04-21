0c9dfda78f7c650be54e10382a4d716d3381b092
IainLR <iainrobertzon@gmail.com>
2021-04-21 12:14:57 -0500

    handling drag and click types and events

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -3,9 +3,16 @@ const EventComponent: React.FC = () => {
     console.log(e)
   }
 
+  const handleDrag = (e: React.DragEvent<HTMLDivElement>) => {
+    console.log(e)
+  }
+
   return (
     <div>
       <input type='text' onChange={(e) => handleChange(e)} />
+      <div draggable onDragStart={(e) => handleDrag(e)}>
+        Drag!
+      </div>
     </div>
   )
 }
